4cc01cff76481d2a015816c9c66ed87eef475d8e
kesutraB <99909890+kesutraB@users.noreply.github.com>
2022-05-26 11:14:47 +0200

    initial commit

---
+++ FilrManager3000/FilrManager3000/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using BetterConsoleTables;
+
+namespace FilrManager3000
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var directoryPath = ReadValue("Enter directory", @"C:\Windows\System32");
+			var files = Directory.GetFiles(directoryPath);
+			var fileInfos = files.Select(x => new FileInfo(x)).ToList();
+
+			var extensions = ReadValue("Enter extensions", "");
+			var extensionList = extensions.Select(x => x.).ToList();
+
+		}
+
+		private static string ReadValue(string label, string defaultValue)
+		{
+			Console.Write($"{label}, (default: {defaultValue}): ");
+			string value = Console.ReadLine();
+			if (value == "")
+				return defaultValue;
+
+			return value;
+		}
+	}
+}
